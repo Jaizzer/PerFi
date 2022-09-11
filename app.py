150166e05b7b9df5e3c07df397f9d729e1910561
Florenz Jaizzer Calderon <jaizzer@Florenz.local>
2022-09-11 15:08:51 +0800

    awit

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("default.html")
 
 
 @app.route("/logout")
